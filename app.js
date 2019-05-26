6ab87bd8a1fd4a9aae4ec699a01d30e160b787b2
kingkill85 <michaelkusche85@yahoo.com>
2019-05-26 19:00:45 +0200

    first alpha

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const ws = require('ws');
+const puppeteer = require('puppeteer');
+
+const port = 8080;
+const app = express();
+const server = app.listen(port, () => console.log(`Listening on ${ port }`));
+
+app.use((req, res) => res.sendFile('/index.html'));
+
+
+const wss = new ws.Server({ server })
+
+wss.on('connection', function (ws) {
+  
+});
+
+listenTips('my_mongolian_slut');
+listenTips('keokistar');
+listenTips('magical_ramona');
+
+function listenTips(modelName) {
+  (async () => {
+    try {
+      const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      });
+      const [page] = await browser.pages();
+
+      async function mutationListener(addedText) {
+        if (/(.*?) tipped \d+ token/g.test(addedText) &&
+          addedText.indexOf(':') == -1) {
+
+          var tipStrArr = addedText.split(' ');
+
+          if(tipStrArr && tipStrArr.length > 0){
+            var logstring = `[${modelName}] ${tipStrArr[0]}: ${tipStrArr[2]}`;
+            console.log(logstring);
+            if (wss) {
+              wss.clients.forEach((client) => {
+                client.send(logstring);
+              });
+            }
+          }
+        }
+      }
+
+      page.exposeFunction('mutationListener', mutationListener);
+      await page.goto('https://chaturbate.eu/' + modelName + '/');
+      await page.waitForSelector('.chat-box');
+      await page.evaluate(() => {
+        const observerTarget = document.querySelector('div.chat-list');
+        const mutationObserver = new MutationObserver((mutationsList) => {
+
+          for (const mutation of mutationsList) {
+            const { removedNodes, addedNodes } = mutation;
+            mutationListener(addedNodes[0].innerText);
+          }
+        });
+        mutationObserver.observe(
+          observerTarget,
+          { childList: true },
+        );
+      });
+    } catch (e) {
+      listenTips(modelName);
+      console.log(e);
+    }
+  })();
+}
