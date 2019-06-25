0b2c49fb1355509d8527eed6a060d70115def1ee
kingkill85 <michaelkusche@live.de>
2019-06-26 00:12:44 +0200

    added vue client side

---
+++ client/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <!--div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div-->
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#"><b-img src="./assets/logo.png" height="50" alt="CB-Alerts"></b-img></b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/about">About</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template slot="button-content"><em>User</em></template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <div class="container-fluid content">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<style>
+  .content {
+    margin-top: 10px; 
+  }
+
+  #app {
+    background-color: darkgray; 
+  }
+
+  html {
+    background-color: darkgray; 
+  }
+</style>
